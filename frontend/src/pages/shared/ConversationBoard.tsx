e7fddc6419d30be3815bc0f758e9c3628ffb59ba
Swetha Nethmi <snethmi18@gmail.com>
2025-09-15 05:56:01 +0530

    create folder structure

---
+++ frontend/src/pages/shared/ConversationBoard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ConversationBoard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ConversationBoard
---
+++ frontend/src/pages/writer/WriterDashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WriterDashboard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default WriterDashboard
